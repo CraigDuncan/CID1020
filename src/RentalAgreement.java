b107a0304bba7c6c716c11539a3deb47a14b223f
CraigDuncan <butterfiy71@aol.com>
2020-10-05 18:14:01 -0500

    Agreement printing is now properly formated.

--- src/RentalAgreement.java
+++ src/RentalAgreement.java
@@ -1,3 +1,6 @@
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
@@ -28,10 +31,28 @@ public class RentalAgreement {
 		this.discountPercent = discountPercent;
 		this.preDiscountCharge = this.dailyRentalCharge * chargeDays;
 		this.discountAmount = Math.round(this.preDiscountCharge * this.discountPercent) / 100.0; //constant 100 is to convert to percent from whole number.
-		this.finalCharge = this.preDiscountCharge - this.discountAmount;
+		this.finalCharge = (this.preDiscountCharge - this.discountAmount);
 	}
 	public String toString() {
-		return this.toolCode + this.toolType + this.toolBrand + this.rentalDays  + this.checkOutDate+ this.dueDate + this.dailyRentalCharge + " " + this.chargeDays +" " + this.discountPercent +" " + this.preDiscountCharge +" " + this.discountAmount +" " + this.finalCharge;
+		NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(); //set up currency formating. Country can be changed and all prints update.
+		String pattern = "MM/dd/yy";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern); // set up date formating 
+		return "Tool code: "+ this.toolCode +
+				"\nTool type: " + this.toolType + 
+				"\nTool brand: " + this.toolBrand +
+				"\nRental days: " + this.rentalDays  +
+				"\nCheck out date: " + simpleDateFormat.format(this.checkOutDate) + 
+				"\nDue date: " + simpleDateFormat.format(this.dueDate) + 
+				"\nDaily rental charge: " + currencyFormat.format(this.dailyRentalCharge) + 
+				"\nCharge days: " + this.chargeDays +
+				"\nPre-discount charge: " + this.discountPercent +"%"+
+				"\nDiscount percent:  " +  currencyFormat.format(this.preDiscountCharge) +
+				"\nDiscount amount:  " +  currencyFormat.format(this.discountAmount) +
+				"\nFinal charge: " +  currencyFormat.format(this.finalCharge);
 	}
 
+	public void printAgreement() {
+		System.out.println(this.toString());
+	}
+	
 }
